e4fffbaa2669eec03b6fd09b2bfbc97b999b07c5
Lochkartenman <lochkartenman@web.de>
2024-05-17 21:33:32 +0200

    Initial commit of images

---
+++ generate.bat
@@ -0,0 +1,69 @@
+del *.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'LAND\nGEAR'"  landing_gear.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'HEAD\nLIGHT'"  head_lights.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'HARD\nPOINT'"  hard_point.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'FLIGHT\nASSIST'"  flight_assist.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'NIGHT\nVISION'"  night_vision.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'CARGO\nSCOOP'"  cargo_scoop.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'ORBIT\nLINES'"  orbit_lines.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'SILENT\nRUNING'"  silent_running.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'HEAT\nSINK'"  heat_sink.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'CHAFF'"  chaff.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'SHIELD\nCELL'"  shield_cell.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'ECM'"  ecm.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'FSD'"  fsd.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'FSD\nCRUISE'"  fsd_supercruise.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'FSD\nJUMP'"  fsd_hyperspace.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'COMBAT\nMODE'"  combat_mode.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'ANALYS\nMODE'"  analysis_mode.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'INTERN'"  internal_panel.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'EXTERN'"  external_panel.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'ROLE'"  role_panel.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'GALAXY\nMAP'"  galaxy_map.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'SYSTEM\nMAP'"  system_map.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'CODEX'"  codex.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'COMMS'"  comms_panel.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'FSS'"  fss.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'WEP'"  weapons.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'ENG'"  engines.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'SYS'"  systems.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'POWER'"  reset_power.png
+
+magick -size 600x600 xc:white -fill black -stroke black -draw "rectangle 0,400 600,600" -font Arial-Narrow -pointsize 150 -gravity center -draw "text 0,-100 'NEXT\nSYSTEM'"  next_system.png
+
+FOR %%f in (*.png) do (
+	magick convert -negate %%f %%~nf-inv.png
+)
+
+FOR %%f in (*.png) DO (
+	magick convert %%f -transparent white %%f
+)
